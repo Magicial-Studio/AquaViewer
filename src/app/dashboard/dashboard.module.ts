ab7fbd3c2d7f7c7e20d18b963245fd917494b3cb
Raspberry-Monster <lele2194512339@outlook.com>
2023-02-17 16:46:23 +0800

    [i18n] Introduce i18n Support For Changelog Dashboard and Importer

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,12 +3,14 @@ import {NgModule} from '@angular/core';
 import {DashboardComponent} from './dashboard.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
-  imports: [
-    MatCardModule,
-    MatButtonModule
-  ],
+    imports: [
+        MatCardModule,
+        MatButtonModule,
+        TranslateModule
+    ],
   exports: [],
   declarations: [DashboardComponent],
   providers: [],
--- src/app/importer/importer.module.ts
+++ src/app/importer/importer.module.ts
@@ -5,18 +5,20 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TranslateModule} from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [ImporterComponent],
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatInputModule,
-    MatCardModule,
-    ReactiveFormsModule,
-    FormsModule,
-  ]
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatInputModule,
+        MatCardModule,
+        ReactiveFormsModule,
+        FormsModule,
+        TranslateModule,
+    ]
 })
 export class ImporterModule {
 }
